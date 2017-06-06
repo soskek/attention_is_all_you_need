adbc31f6485ee0dbfbb2810aee71f09a3092acf6
Sosuke Kobayashi <sosk@preferred.jp>
2017-06-06 16:18:57 +0900

    clean

--- net.py
+++ net.py
@@ -130,28 +130,32 @@ class ConvGLUDecoder(chainer.Chain):
             (x.shape[0], x.shape[1], self.width - 1, 1), dtype=x.dtype)
         base_x = x
         z = F.squeeze(z, axis=3)
-        # conv_mask = mask[:, :, 0][:, None, :, None]
-        # (batch, 1, dec_l, 1)
         # Note: these behaviors of input, output, and attention result
         # may refer to the code by authors, which looks little different
         # from the paper's saying.
         for conv_name, preatt_name in zip(self.conv_names, self.preatt_names):
+            # Calculate Output of GLU
             out = getattr(self, conv_name)(
                 F.concat([pad, x], axis=2), conv_mask)
-
+            # Calcualte Output of Attention using Output of GLU
             preatt = seq_linear(getattr(self, preatt_name), out)
             query = base_x + preatt
             query = F.squeeze(query, axis=3)
             c = self.attend(query, z, ze, mask) * att_scale
+            # Merge Them in Redidual Calculation and Scaling
             x = (x + (c + out) * scale05) * scale05
 
         return x
 
     def attend(self, query, key, value, mask, minfs=None):
-        # TODO reshape
-        # (b, units, dec_xl) (b, units, enc_l) (b, units, dec_l, enc_l)
-        pre_a = F.batch_matmul(query, key, transa=True)
-        # (b, dec_xl, enc_l)
+        """
+        Input shapes:
+            q=(b, units, dec_l), k=(b, units, enc_l),
+            v=(b, units, dec_l, enc_l), m=(b, dec_l, enc_l)
+        """
+
+        # Calculate Attention Scores with Mask for Zero-padded Areas
+        pre_a = F.batch_matmul(query, key, transa=True)  # (b, dec_l, enc_l)
         minfs = self.xp.full(pre_a.shape, -np.inf, pre_a.dtype) \
             if minfs is None else minfs
         pre_a = F.where(mask, pre_a, minfs)
@@ -159,18 +163,18 @@ class ConvGLUDecoder(chainer.Chain):
         # if values in axis=2 are all -inf, they become nan. thus do re-mask.
         a = F.where(self.xp.isnan(a.data),
                     self.xp.zeros(a.shape, dtype=a.dtype), a)
-        reshaped_a = a[:, None]
-        # (b, 1, dec_xl, enc_l)
+        reshaped_a = a[:, None]  # (b, 1, dec_xl, enc_l)
+
+        # Calculate Weighted Sum
         pre_c = F.broadcast_to(reshaped_a, value.shape) * value
-        c = F.sum(pre_c, axis=3, keepdims=True)
-        # (b, units, dec_xl, 1)
+        c = F.sum(pre_c, axis=3, keepdims=True)  # (b, units, dec_xl, 1)
         return c
 
 
 class Seq2seq(chainer.Chain):
 
     def __init__(self, n_layers, n_source_vocab, n_target_vocab, n_units,
-                 max_length=128, dropout=0.2):
+                 max_length=128, dropout=0.2, width=3):
         init_emb = chainer.initializers.Normal(0.1)
         init_out = VarInNormal(1.)
         super(Seq2seq, self).__init__(
@@ -182,29 +186,31 @@ class Seq2seq(chainer.Chain):
                                        initialW=init_emb),
             embed_position_y=L.EmbedID(max_length, n_units,
                                        initialW=init_emb),
-            encoder=ConvGLUEncoder(n_layers, n_units, 3, dropout),
-            decoder=ConvGLUDecoder(n_layers, n_units, 3, dropout),
+            encoder=ConvGLUEncoder(n_layers, n_units, width, dropout),
+            decoder=ConvGLUDecoder(n_layers, n_units, width, dropout),
             W=L.Linear(n_units, n_target_vocab, initialW=init_out),
         )
         self.n_layers = n_layers
         self.n_units = n_units
         self.n_target_vocab = n_target_vocab
         self.max_length = max_length
+        self.width = width
         self.dropout = dropout
 
     def __call__(self, x_block, y_in_block, y_out_block, get_prediction=False):
         batch, x_length = x_block.shape
         batch, y_length = y_in_block.shape
 
-        # Embed
+        # Embed Words
         ex_block = sentence_block_embed(self.embed_x, x_block)
         ey_block = sentence_block_embed(self.embed_y, y_in_block)
+
+        # Embed Positions
         max_len = max(x_length, y_length)
         position_block = self.xp.broadcast_to(
             self.xp.clip(
                 self.xp.arange(max_len), 0, self.max_length - 1)[None, ],
             (batch, max_len)).astype('i')
-
         px_block = sentence_block_embed(
             self.embed_position_x, position_block[:, :x_length])
         py_block = sentence_block_embed(
@@ -212,12 +218,12 @@ class Seq2seq(chainer.Chain):
         ex_block += px_block
         ey_block += py_block
 
-        # Encode
+        # Encode Sources
         ex_mask = self.xp.broadcast_to(
             x_block[:, None, :, None] >= 0, ex_block[:, :, :, None].shape)
         z_block = self.encoder(ex_block[:, :, :, None], ex_mask)
 
-        # Prepare attention
+        # Prepare Attention to Sources
         z_block = gradient_multiplier(z_block, 1. / self.n_layers / 2)
         ze_block = F.broadcast_to(
             F.transpose(
@@ -226,43 +232,34 @@ class Seq2seq(chainer.Chain):
         z_mask = (x_block[:, None, :] >= 0) * \
             (y_in_block[:, :, None] >= 0)
 
-        # Decode (target-encode before output)
+        # Encode Targets with Sources (Decode without Output)
         ey_mask = self.xp.broadcast_to(
             y_in_block[:, None, :, None] >= 0, ey_block[:, :, :, None].shape)
         h_block = self.decoder(ey_block[:, :, :, None],
                                z_block, ze_block, z_mask, ey_mask)
         h_block = F.squeeze(h_block, axis=3)
-
-        # It is faster to concatenate data before calculating loss
-        # because only one matrix multiplication is called.
         assert(h_block.shape == (batch, self.n_units, y_length))
 
         if get_prediction:
-            """
-            concat_h_block = F.transpose(h_block, (0, 2, 1)).reshape(
-                (batch * y_length, self.n_units))
-            concat_h_block = F.dropout(concat_h_block, ratio=self.dropout)
-            concat_pred_block = self.W(concat_h_block)
-
-            pred_block = concat_pred_block.reshape(
-                (batch, y_length, self.n_target_vocab))
-            return pred_block
-            """
             pred_tail = self.W(
                 F.dropout(h_block[:, :, -1], ratio=self.dropout))
             return pred_tail
         else:
+            # Output (all together at once for efficiency)
             concat_h_block = F.transpose(h_block, (0, 2, 1)).reshape(
                 (batch * y_length, self.n_units))
             concat_h_block = F.dropout(concat_h_block, ratio=self.dropout)
             concat_pred_block = self.W(concat_h_block)
 
+            # Calculate Loss, Accuracy, Perplexity
             concat_y_out_block = y_out_block.reshape((batch * y_length))
             loss = F.softmax_cross_entropy(
                 concat_pred_block, concat_y_out_block, reduce='mean')
             accuracy = F.accuracy(
                 concat_pred_block, concat_y_out_block, ignore_label=-1)
             perp = self.xp.exp(loss.data)
+
+            # Report the Values
             rep = {'loss': loss.data, 'acc': accuracy.data, 'perp': perp}
             reporter.report(rep, self)
             return loss
